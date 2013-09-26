95e95273f30f1b8f4bde3ec1ea79a7553866d8b6
Joseph Lin <puyan.tw@gmail.com>
2013-09-27 01:38:47 +0800

    Write a download demo app

--- DemoDownloadAndReplaced/src/main/java/tw/puyan/demodownloadandreplaced/MainActivity.java
+++ DemoDownloadAndReplaced/src/main/java/tw/puyan/demodownloadandreplaced/MainActivity.java
@@ -1,8 +1,25 @@
 package tw.puyan.demodownloadandreplaced;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.PowerManager;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +29,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Button btnUpdate=(Button) findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DownloadTask task=new DownloadTask();
+                task.execute("http://220.135.205.124:8080/share.cgi?ssid=08Au2j8");
+            }
+        });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +50,113 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+
+        ProgressDialog mProgressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog=new ProgressDialog(MainActivity.this, AlertDialog.THEME_HOLO_LIGHT);
+            mProgressDialog.setMessage("A message");
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            mProgressDialog.setCancelable(true);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            // take CPU lock to prevent CPU from going off if the user
+            // presses the power button during download
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    getClass().getName());
+            wl.acquire();
+
+            try {
+                InputStream input = null;
+                OutputStream output = null;
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(sUrl[0]);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+
+                    // expect HTTP 200 OK, so we don't mistakenly save error report
+                    // instead of the file
+                    if (connection.getResponseCode() != HttpURLConnection.HTTP_OK)
+                        return "Server returned HTTP " + connection.getResponseCode()
+                                + " " + connection.getResponseMessage();
+
+                    // this will be useful to display download percentage
+                    // might be -1: server did not report the length
+                    int fileLength = connection.getContentLength();
+
+                    // download the file
+                    input = connection.getInputStream();
+                    output = new FileOutputStream(getExternalFilesDir("Download")+"/updateOne.apk");
+
+                    byte data[] = new byte[4096];
+                    long total = 0;
+                    int count;
+                    while ((count = input.read(data)) != -1) {
+                        // allow canceling with back button
+                        if (isCancelled())
+                            return null;
+                        total += count;
+                        // publishing the progress....
+                        if (fileLength > 0) // only if total length is known
+                            publishProgress((int) (total * 100 / fileLength));
+                        output.write(data, 0, count);
+                    }
+                } catch (Exception e) {
+                    return e.toString();
+                } finally {
+                    try {
+                        if (output != null)
+                            output.close();
+                        if (input != null)
+                            input.close();
+                    }
+                    catch (IOException ignored) { }
+
+                    if (connection != null)
+                        connection.disconnect();
+                }
+            } finally {
+                wl.release();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            // if we get here, length is known, now set indeterminate to false
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgress(progress[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mProgressDialog.dismiss();
+            if (result != null)
+                Toast.makeText(MainActivity.this, "Download error: " + result, Toast.LENGTH_LONG).show();
+            else{
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                intent.setDataAndType(Uri.fromFile(new File(getExternalFilesDir("Download")+"/updateOne.apk")), "application/vnd.android.package-archive");
+
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+
+
+
     
 }
